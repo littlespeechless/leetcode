7cff505f94ee8ca55c88ab8c9a376db1822bbcab
ZhengYu Wu <zhenwu@cs.stonybrook.edu>
2021-01-14 19:50:48 -0500

    - 143. Reorder List

---
+++ 143/main.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head:
+            return
+
+        # find the mid point
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # reverse the second half in-place
+        # 4 -> 5 -> 6 = 6 -> 5 -> 4
+        pre, node = None, slow
+        while node:
+            pre, node.next, node = node, pre, node.next
+
+        # Merge in-place; Note : the last node of "first" and "second" are the same
+        first, second = head, pre
+        while second.next:
+            first.next, first = second, first.next
+            second.next, second = first, second.next
+        return
+
+
+if __name__ == '__main__':
+    head = ListNode(1)
+    cursor = head
+    for i in range(2, 5, 1):
+        cursor.next = ListNode(i)
+        cursor = cursor.next
+    Solution().reorderList(head)
+    print(head)
